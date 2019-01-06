11fe6408a24b5ed2e7aa47d348d7c59fd8b00fd8
joel-ace <joelakwes@gmail.com>
2019-01-06 11:44:54 +0100

    - add pagination to display generated numbers

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
+import ReactPaginate from 'react-paginate';
 
 import GenerateNumberForm from './GenerateNumberForm';
-import { generateNumbers } from '../utils';
+import { generateNumbers, paginate } from '../utils';
 import DisplayGeneratedNumbers from './DisplayGeneratedNumbers';
 
 const GENERATE_NUMBER_LIMIT = 10000;
+const PAGINATION_LIMIT = 20;
+
+const defaultPaginationData = {
+	data: [],
+	totalCount: 0,
+	pageCount: 0,
+};
 
 class Content extends Component {
 	state = {
 		phoneNumbers: [],
 		quantity: 1,
 		error: '',
-	}
+		paginationData: defaultPaginationData,
+	};
 
 	handleGeneratePhoneNumbers = (quantity) => {
 		if (quantity > 0 && quantity <= GENERATE_NUMBER_LIMIT) {
@@ -19,34 +28,51 @@ class Content extends Component {
 			return this.setState({
 				phoneNumbers: generatedPhoneNumbers,
 				error: '',
+				paginationData: paginate(PAGINATION_LIMIT, 1, generatedPhoneNumbers),
 			});
 		};
 
 		return this.setState({
 			phoneNumbers: [],
 			error: `Please enter a number between 1 and ${GENERATE_NUMBER_LIMIT}`,
+			paginationData: defaultPaginationData,
 		});
-
 	};
 
 	handleQuantityChange = (event) => {
 		const { target: { value: quantity } } = event;
+
+		if (!quantity) {
+			return false;
+		}
+
 		this.setState({
-			quantity,
+			quantity: parseInt(quantity, 10),
 		});
+	};
+
+	handlePageClick = (data) => {
+		const { selected } = data;
+		const { phoneNumbers } = this.state;
+
+		const paginationData = paginate(PAGINATION_LIMIT, parseInt(selected + 1, 10), phoneNumbers);
+		this.setState({
+			paginationData,
+		});
+
 	};
 
 	renderErrorMessage = () => {
 		const { error } = this.state;
 		return error && <div className="alert alert-danger">{error}</div>;
-	}
+	};
 
 	renderGeneratedNumbers = (phoneNumbers) => {
 		return (phoneNumbers.length > 0) && <DisplayGeneratedNumbers phoneNumbers={phoneNumbers} />
-	}
+	};
 
 	render () {
-		const { quantity, phoneNumbers } = this.state;
+		const { quantity, paginationData: { data: phoneNumbers, pageCount } } = this.state;
 
 		return (
 			<div className="container">
@@ -59,6 +85,20 @@ class Content extends Component {
 						/>
 						{ this.renderErrorMessage() }
 						{ this.renderGeneratedNumbers(phoneNumbers) }
+						{
+							(phoneNumbers.length > 0) &&
+							<ReactPaginate previousLabel={"previous"}
+								nextLabel={"next"}
+								breakLabel={"..."}
+								breakClassName={"break-me"}
+								pageCount={pageCount}
+								marginPagesDisplayed={2}
+								pageRangeDisplayed={5}
+								onPageChange={this.handlePageClick}
+								containerClassName={"pagination"}
+								subContainerClassName={"pages pagination"}
+								activeClassName={"active"} />
+						}
 					</div>
 				</div>
 			</div>
