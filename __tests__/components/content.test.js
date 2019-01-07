b9ad01648790a91ac2ca7c042f5c47cacb315c55
joel-ace <joelakwes@gmail.com>
2019-01-07 11:50:18 +0100

    - add sort number

--- __tests__/components/content.test.js
+++ __tests__/components/content.test.js
@@ -14,6 +14,7 @@ describe('Content component', () => {
   const handleQuantityChangeSpy = jest.spyOn(contentComponent.instance(), 'handleQuantityChange');
 	const renderErrorMessageSpy = jest.spyOn(contentComponent.instance(), 'renderErrorMessage');
 	const handlePageClickSpy = jest.spyOn(contentComponent.instance(), 'handlePageClick');
+	const sortNumbersSpy = jest.spyOn(contentComponent.instance(), 'sortNumbers');
 
 	contentComponent.update();
 	contentComponent.instance().forceUpdate();
@@ -46,11 +47,37 @@ describe('Content component', () => {
 		});
 	});
 
+	it('should call sortNumbers function when the asc or desc buttons are clicked', (done) => {
+		const sortButton = contentComponent.find('button.sort-asc');
+		sortButton.simulate('click');
+
+		setImmediate(() => {
+			expect(sortNumbersSpy).toHaveBeenCalled();
+			done();
+		});
+	});
+
+	it('should properly sort the numbers when the asc or desc buttons are clicked', (done) => {
+		const descSortButton = contentComponent.find('button.sort-desc');
+		descSortButton.simulate('click');
+
+		const phoneNumbers = contentComponent.state('paginationData').data;
+		const phoneNumberCount = phoneNumbers.length - 1;
+
+		setImmediate(() => {
+			expect(parseInt(phoneNumbers[phoneNumberCount], 10)).toBeLessThan(parseInt(phoneNumbers[phoneNumberCount - 2], 10));
+			done();
+		});
+	});
+
   it('should display an error message if quantity entered is less than 1 or more than 10000 is entered', (done) => {
 		contentComponent.setState({ quantity: 0 });
 		button.simulate('click');
 
+		const errorDiv = contentComponent.find('.alert-danger');
+
 		setImmediate(() => {
+			expect(errorDiv.text()).toBe('Please enter a number between 1 and 10000');
 			expect(renderErrorMessageSpy).toHaveBeenCalled();
 			expect(contentComponent.state('error')).toBe('Please enter a number between 1 and 10000');
 			done();
--- src/components/GenerateNumberForm.jsx
+++ src/components/GenerateNumberForm.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 
 const GenerateNumberForm = ({ generatePhoneNumbers, quantity, handleQuantityChange }) => (
-	<form className="form-inline">
+	<div className="form-inline">
 		<div style={{ margin: '20px auto', textAlign: 'center' }}>
-			<h4 style={{ marginBottom: 20 }}>Enter the quantity of phone numbers to generate</h4>
 			<div className="form-group" style={{ marginRight: 20 }}>
 				<input type="number" min={1} max={10000} onChange={handleQuantityChange} value={quantity} className="form-control input-number-quantity"/>
 			</div>
 			<button onClick={() => generatePhoneNumbers(quantity)} type="button" className="btn btn-primary generate-number-button">Generate numbers</button>
 		</div>
-	</form>
+	</div>
 );
 
 export default GenerateNumberForm;
